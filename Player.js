afa4b6440ab8ed2e1796c1bb0569322adf36c0da
Miklós Kondász <miklos.kondasz@gmail.com>
2019-05-20 12:00:03 +0200

    squared bracket holecards

--- Player.js
+++ Player.js
@@ -1,20 +1,20 @@
 class Player {
   static get VERSION() {
-    return '1.8';
+    return '2.2';
   }
 
   static betRequest(gameState, bet) {
-    let holecards;
+    let holecards = [];
     console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+gameState+"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbb");
-    for (let player of gameState.players)
+
+    for (let player of gameState.players) {
       if (player.name == "TheRealKordaGyuri") {
-        {
+
           for (let i = 0; i < player.hole_cards.length; i++) {
             holecards[i] = player.hole_cards[i];
           }
-        }
-
       }
+    }
     if (holecards[0].rank == "A" || holecards[1].rank == "A") {
       bet(100);
     } else {
