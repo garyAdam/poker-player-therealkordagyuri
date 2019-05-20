570a781f019756fde3b7225215b895b9bd9f77c8
garyAdam <rapture047@gmail.com>
2019-05-20 11:35:02 +0200

    test holecardcheck

--- Player.js
+++ Player.js
@@ -1,10 +1,26 @@
 class Player {
   static get VERSION() {
-    return '1.6';
+    return '1.7';
   }
 
   static betRequest(gameState, bet) {
-    bet(5)
+    let holecards;
+    for (let player of gameState.game_state.players)
+      if (player.name == "TheRealKordaGyuri") {
+        {
+          for (let i = 0; i < player.hole_cards.length; i++) {
+            holecards[i] = player.hole_cards[i];
+          }
+        }
+
+      }
+    if (holecards[0].rank == "A" || holecards[1].rank == "A") {
+      bet(100);
+    } else {
+
+
+      bet(5)
+    }
   }
 
   static showdown(gameState) {
