5d83be967bc09c94880fdd3b410bdc3672d10274
Miklós Kondász <miklos.kondasz@gmail.com>
2019-05-20 14:28:12 +0200

    two big bois

--- Player.js
+++ Player.js
@@ -1,7 +1,7 @@
 class Player {
 
   static get VERSION() {
-    return '3.11';
+    return '3.32';
   }
 
   static betRequest(gameState, bet) {
@@ -33,15 +33,21 @@ class Player {
             //minimum raise
             bet(parseInt(gameState.current_buy_in) - parseInt(gameState.players[gameState.in_action].bet) + parseInt(gameState.minimum_raise));
           }
-        } else {
+        // two big boi
+        } else if (holecards[0].rank > "9" && holecards[1].rank > "9") {
+        // call
+            bet(parseInt(gameState.current_buy_in) - parseInt(gameState.players[gameState.in_action].bet));
+        }
+        //no big boi && no pair
+        else {
           bet(0);
         }
-      } else {
+      }
+
+      else {
         bet(parseInt(gameState.current_buy_in) - parseInt(gameState.players[gameState.in_action].bet));
       }
 
-
-      bet(5)
     } catch (e) {
       console.error(e + "catched");
       bet(10)
