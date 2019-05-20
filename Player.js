5838c64df3d234523bd2bd3d05cc9d92d3aef49b
garyAdam <rapture047@gmail.com>
2019-05-20 11:50:55 +0200

    4

--- Player.js
+++ Player.js
@@ -6,22 +6,7 @@ class Player {
   static betRequest(gameState, bet) {
     let holecards;
     console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+gameState+"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbb");
-    for (let player of gameState.players)
-      if (player.name == "TheRealKordaGyuri") {
-        {
-          for (let i = 0; i < player.hole_cards.length; i++) {
-            holecards[i] = player.hole_cards[i];
-          }
-        }
-
-      }
-    if (holecards[0].rank == "A" || holecards[1].rank == "A") {
-      bet(100);
-    } else {
-
-
-      bet(5)
-    }
+    bet(1000);
   }
 
   static showdown(gameState) {
