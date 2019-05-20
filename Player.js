0578ab53255ea345455775a4f475e7da0fe34775
Sütő Bence 'Owen <ssben636@gmail.com>
2019-05-20 11:50:47 +0200

    change bet

--- Player.js
+++ Player.js
@@ -1,27 +1,10 @@
 class Player {
   static get VERSION() {
-    return '1.8';
+    return '2.0';
   }
 
   static betRequest(gameState, bet) {
-    let holecards;
-    console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+gameState+"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbb");
-    for (let player of gameState.players)
-      if (player.name == "TheRealKordaGyuri") {
-        {
-          for (let i = 0; i < player.hole_cards.length; i++) {
-            holecards[i] = player.hole_cards[i];
-          }
-        }
-
-      }
-    if (holecards[0].rank == "A" || holecards[1].rank == "A") {
-      bet(100);
-    } else {
-
-
-      bet(5)
-    }
+    bet(100)
   }
 
   static showdown(gameState) {
