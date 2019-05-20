8c66af40dcc4433cf7a92269884563b4535af9c7
garyAdam <rapture047@gmail.com>
2019-05-20 10:19:37 +0200

    testbet

--- Player.js
+++ Player.js
@@ -1,10 +1,10 @@
 class Player {
   static get VERSION() {
-    return '1.0';
+    return '1.1';
   }
 
   static betRequest(gameState, bet) {
-    bet(2);
+    bet(200);
   }
 
   static showdown(gameState) {
