0c768cc5fb542155d27e18049286781c06ddcd3a
garyAdam <rapture047@gmail.com>
2019-05-20 14:52:10 +0200

    community

--- Player.js
+++ Player.js
@@ -1,7 +1,7 @@
 class Player {
 
   static get VERSION() {
-    return '3.32';
+    return '3.42';
   }
 
   static betRequest(gameState, bet) {
@@ -28,30 +28,49 @@ class Player {
           // small pair
           if (holecards[0].rank < "10") {
             //call
-            bet(parseInt(gameState.current_buy_in) - parseInt(gameState.players[gameState.in_action].bet));
+            this.call(bet, gameState);
           } else {
             //minimum raise
-            bet(parseInt(gameState.current_buy_in) - parseInt(gameState.players[gameState.in_action].bet) + parseInt(gameState.minimum_raise));
+            this.raise(bet, gameState);
           }
-        // two big boi
+          // two big boi
         } else if (holecards[0].rank > "9" && holecards[1].rank > "9") {
-        // call
-            bet(parseInt(gameState.current_buy_in) - parseInt(gameState.players[gameState.in_action].bet));
+          // call
+          this.call(bet, gameState)
         }
         //no big boi && no pair
         else {
           bet(0);
         }
-      }
-
-      else {
+      } else {
         bet(parseInt(gameState.current_buy_in) - parseInt(gameState.players[gameState.in_action].bet));
       }
+    else
+      {
+        if (communityCards) {
+          for (let card of communityCards) {
+            if (holecards[0].rank == card.rank || holecards[1].rank == card.rank) {
+              this.raise(bet, gameState);
+
+            }
+          }
+        }
+        this.call(bet, gameState);
+      }
+
 
     } catch (e) {
       console.error(e + "catched");
       bet(10)
     }
+  }
+
+  static call(bet, gameState) {
+    bet(parseInt(gameState.current_buy_in) - parseInt(gameState.players[gameState.in_action].bet));
+  }
+
+  static raise(bet, gameState) {
+    bet(parseInt(gameState.current_buy_in) - parseInt(gameState.players[gameState.in_action].bet) + parseInt(gameState.minimum_raise));
   }
 
   static showdown(gameState) {
